48c1c35972904e2b403aa80f30e46fee404fdc8d
niu <niu@linux-u8sv.site>
2010-02-26 22:34:07 +0300

    box little refactor

--- box.h
+++ box.h
@@ -6,8 +6,18 @@
 //currently unused
 #define BIndex int
 
-typedef struct boxtemp {uint (*hash)(uint , int lvl); void (*levelAlloc)(struct boxtemp* b,int lvl); uint index; int level,maxLevel; void* data; int dataSize; struct boxtemp* nextlvl; 
-int levelSize; int set; int numUsed} Box;
+typedef struct boxtemp 
+{
+	uint (*hash)(uint , int lvl); 
+	void (*levelAlloc)(struct boxtemp* b,int lvl); 
+	uint index; 
+	int level,maxLevel; 
+	void* data; 
+	int dataSize; 
+	struct boxtemp* nextlvl; 
+	int levelSize; 
+	int set; 
+	int numUsed} Box;
 
 uint hash_native(uint index, int lvl);
 void levelAlloc_native(struct boxtemp* b,int lvl);
