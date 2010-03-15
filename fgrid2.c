ae2a2e5870a4380bc3efd540b1f70bd1aa6f1362
arch <arch@MacBook-Ivan-Komissarov.local>
2010-03-16 00:14:18 +0300

    Again some documentation

--- fgrid2.c
+++ fgrid2.c
@@ -4,7 +4,9 @@
     \fn int fgrid2_native(int x, int y, fgrid2 *fg)
     \brief Simple mapping function.
  
-    Converts \a x and \a y coordinates to MPI comm rank. Fills lines (\a y coordinate) first. NOTE: \a x are rows and \a y are columns. If you want to use another mapping function, reimplement it and pass to field \a reverse in fgrid2 structure
+    Converts \a x and \a y coordinates to MPI comm rank. Fills lines (\a y coordinate) first. NOTE: \a x are rows and \a y are columns. If you want to use another mapping function, reimplement it and pass to field \a reverse in fgrid2 structure.
+    Эта функция используется в паре с функцией fgrid2_reverse(fgrid2 *g) и вместе они задают топологию
+    \sa fgrid2_reverse
  */
 int fgrid2_native(int x, int y, fgrid2 *fg)
 {
@@ -19,6 +21,7 @@ int fgrid2_native(int x, int y, fgrid2 *fg)
     \param [in,out] g Структура, описывающая топологию
  
     Функция берёт MPI rank, указанный в структуре \a g, и записывает координаты сетки в эту же структуру
+    \sa fgrid2_native
  */
 void fgrid2_reverse(fgrid2 *g)
 {
@@ -28,23 +31,46 @@ void fgrid2_reverse(fgrid2 *g)
 	g->y = g->id % g->w;
 }
 
+/*!
+ \fn void fgrid2CreateXLine(fgrid2 * g)
+ \brief Creates communicator group for line
+ 
+ Выделено в отдельную функцию для удобства
+ \sa fgrid2CreateYLine
+ */
 void fgrid2CreateXLine(fgrid2 * g)
 {
 	assert(g);
 	
 	MPI_Comm_split(g->comm, g->x, 0, &(g->xLine));
-	MPI_Comm_rank(g->xLine,&(g->xLineSelf));
+	MPI_Comm_rank(g->xLine, &(g->xLineSelf));
 }
 
+/*!
+ \fn void fgrid2CreateYLine(fgrid2 * g)
+ \brief Creates communicator group for row
+ 
+ Выделено в отдельную функцию для удобства
+ \sa fgrid2CreateXLine
+ */
 void fgrid2CreateYLine(fgrid2 * g)
 {
 	assert(g);
 	
 	MPI_Comm_split(g->comm, g->y, 0, &(g->yLine));
-	MPI_Comm_rank(g->yLine,&(g->yLineSelf));
+	MPI_Comm_rank(g->yLine, &(g->yLineSelf));
 }
 
-void fgrid2FromRange(fgrid2 * g,MPI_Comm comm,int x, int y)
+
+/*!
+ \fn void fgrid2FromRange(fgrid2 * g,MPI_Comm comm, int x, int y)
+ \brief Creates two-dimensional grid with sizes \a s and \a y and with MPI communicator \a comm.
+ 
+ Количество процессов в коммуникаторе должно совпадать с размером сетки (произведение \a x на \a y)
+ Задаёт базовые параметры, как то: ширина и высота. При вызове создается копия коммуникатора \a comm (эта операция достаточно долгая).
+ В простейшей реализации используются функции fgrid2_native(int x, int y, fgrid2 *fg) и fgrid2CreateYLine(fgrid2 * g)
+ */
+void fgrid2FromRange(fgrid2 * g, MPI_Comm comm, int x, int y)
 {
 	assert(g);
 	assert((x>0) && (y>0));
@@ -57,30 +83,42 @@ void fgrid2FromRange(fgrid2 * g,MPI_Comm comm,int x, int y)
 	g->h=x;
 	
 	MPI_Comm_dup(comm, &(g->comm));
-	MPI_Comm_rank(g->comm,&(g->id));
+	MPI_Comm_rank(g->comm, &(g->id));
 	
 	g->topo.obj=NULL;
 	g->topo.type=Tnone;
 	
 	g->reverse=(void(*)(fgrid2 *g)) (fgrid2_reverse);
 	g->reverse(g);
-	g->map=(int(*)(int,int,fgrid2 *)) (fgrid2_native);
+	g->map=(int(*)(int, int, fgrid2 *)) (fgrid2_native);
 	
 	fgrid2CreateXLine(g);
 	fgrid2CreateYLine(g);
 	
 }
 
+/*!
+ \fn void fgrid2FromNative(fgrid2 * g)
+ \brief Creates grid from native parameters.
+ 
+ Является наиболее простой и быстрой функцией создания топологической сетки. Используются наперед заданные параметры, близкие к железу.
+ */
 void fgrid2FromNative(fgrid2 * g)
 {
 	assert(g);
 	
-	fgrid2FromRange(g,MPI_COMM_WORLD,nativeX,nativeY);
+	fgrid2FromRange(g, MPI_COMM_WORLD, nativeX, nativeY);
 	
 	g->topo.obj=NULL;
 	g->topo.type=Tnone;
 }
 
+/*!
+ \fn void fgrid2Free(fgrid2 * g)
+ \brief Frees \a g structure
+ 
+ При освобождении структуру все поля становятся равными -1, что позволяет отловить ошибку при повторном использовании очищенной структры.
+ */
 void fgrid2Free(fgrid2 * g)
 {
 	assert(g);
@@ -103,7 +141,18 @@ void fgrid2Free(fgrid2 * g)
 	g->topo.obj=NULL;
 }
 
-void fgrid2Slice(fgrid2 * g,fgrid2 * ng, int dim, int sliceIndex)
+/*!
+ \fn void fgrid2Slice(fgrid2 * g, fgrid2 * ng, int dim, int sliceIndex)
+ \brief Creates new grid from existing one by slicing it.
+ \param[in] g Existing grid
+ \param[out] ng Returned value of a new grid
+ \param[in] dim If \a dim == 0 sliced by X coordinate, if \a dim != 0 grid is sliced by y
+ \param[in] sliceIndex Index by which grid is sliced (sliceIndex included into second topology)
+
+ Элемент с номером sliceIndex вхожит во вторую топологию.
+ Функция возвращает только одну из 2х топологий, в зависимости от того, в какой из них находится текущий процесс. В новую топологию устанавливается ссылка на старую.
+ */
+void fgrid2Slice(fgrid2 * g, fgrid2 * ng, int dim, int sliceIndex)
 {
 	assert(g);
 	assert(ng);
@@ -144,15 +193,22 @@ void fgrid2Slice(fgrid2 * g,fgrid2 * ng, int dim, int sliceIndex)
 	ng->reverse(ng);
 	ng->map=g->map;
 	
-	//!!!!!!!!!!lines
+	// !!!!!!!!!!lines
 	fgrid2CreateXLine(ng);
 	fgrid2CreateYLine(ng);
-	//!!!!!!!!!!untested
+	// !!!!!!!!!!untested
 	
 	
 }
 
-void fgrid2SliceLinear(fgrid2 * g,fgrid2 * ng, int xSlice, int ySlice)
+/*!
+ \fn void fgrid2SliceLinear(fgrid2 * g, fgrid2 * ng, int xSlice, int ySlice)
+ \brief Slices existing grid \a g by \a xSlice and \a ySlice lines and columns
+ 
+ Разделяет сетку по координатам xSlice ySlice.
+ Функция возвращает только одну из 4х топологий, в зависимости от того, в какой из них находится текущий процесс. В новую топологию устанавливается ссылка на старую.
+ */
+void fgrid2SliceLinear(fgrid2 * g, fgrid2 * ng, int xSlice, int ySlice)
 {
 	assert(g);
 	assert(ng);
@@ -176,13 +232,13 @@ void fgrid2SliceLinear(fgrid2 * g,fgrid2 * ng, int xSlice, int ySlice)
 	ng->reverse(ng);
 	ng->map=g->map;
 	
-	//!!!!!!!!!!lines
+	// !!!!!!!!!!lines
 	fgrid2CreateXLine(ng);
 	fgrid2CreateYLine(ng);
-	//!!!!!!!!!!untested
+	// !!!!!!!!!!untested
 }
 
-void fgrid2SliceParts(fgrid2 * g,fgrid2 * ng, int* xParts, int* yParts, int xSize, int ySize)
+void fgrid2SliceParts(fgrid2 * g, fgrid2 * ng, int* xParts, int* yParts, int xSize, int ySize)
 {
 	assert(g);
 	assert(ng);
@@ -210,6 +266,11 @@ void fgrid2SliceParts(fgrid2 * g,fgrid2 * ng, int* xParts, int* yParts, int xSiz
 	free(ySum);
 }
 
+/*!
+ \fn void fgrid2Barrier(fgrid2 * g)
+ \brief Locks barrieir on the whole grid
+ 
+ */
 void fgrid2Barrier(fgrid2 * g)
 {
 	assert(g);
