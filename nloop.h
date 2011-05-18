64ae397d1746d28bb21cc00d999b319a45098502
ABBAPOH <ABBAPOH@nextmail.ru>
2011-05-18 14:37:05 +0400

    MOre fixes

--- nloop.h
+++ nloop.h
@@ -6,17 +6,18 @@
 #include "fgridn.h"
 #include "narray.h"
 
-typedef int (*nloop_work_func)(struct nlooptemp * loop, int index[], void* data);
-typedef int (*nloop_depend_func)(struct nlooptemp * loop, int index[], int indexDep[], void* data);
+typedef struct nlooptemp nloop;
+typedef int (*nloop_work_func)(nloop * loop, int index[], void* data);
+typedef int (*nloop_depend_func)(nloop * loop, int index[], int indexDep[], void* data);
 
-typedef struct nlooptemp 
+struct nlooptemp
 {
 	narray* status;
 	nloop_work_func func;
 	nloop_depend_func depend;
 	int localDone;
 	Topology topo;
-} nloop;
+};
 
 void nloopFromnarray(nloop *loop, Topology topo, int sizes[], int dimsCount, npointer (*map)(int[], narray*), void (*alloc)(narray*), nloop_work_func func, nloop_depend_func depend);
 void nloopFree(nloop *loop);
